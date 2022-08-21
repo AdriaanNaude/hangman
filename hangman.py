9ea39612062abb9a960f438a548b4f8b816d5792
AdriaanNaude <naude5@tutanota.com>
2022-08-21 14:53:53 +0200

    Redesign of code blocks and loop.

--- hangman.py
+++ hangman.py
@@ -4,43 +4,33 @@ import random
 word_list = {
     1:"animal",
     2:"read",
-    3:"man"
+    3:"man",
+    4:"fan"
 }
 
-word = word_list[random.randrange(1,4)]
+word = word_list[random.randrange(1,5)]
 word_length = len(word)
 
 print("The word length is: " + str(word_length))
 
-#point = 0
-guess = 0
-
-
+kans = 0
 
 def r_expresion(user_input, word):
-    regex = rf"^{user_input}$"
+    regex = rf"^{word}$"
 
-    if re.match(regex, word):
+    if re.match(regex, user_input):
         print("Match")
     else:
        print("No match")
+        
 
-while guess < 10:
-    user_stdin = input("Your")
-    
-    r_expresion(user_stdin, word)
+def loop(x):
+    while x < 10:
+        user_init = input("Your guess: ")
+        x+=1
+        if user_init == "q":
+            ptint("Exiting")
+            break
+        r_expresion(user_init, word)
 
-"""
-def resultate(point, guess):
-    if guess == 6:
-        print("guesses done")
-    if point == word_length:
-        print("You win " + point + " " + word)
-
-def keuse():
-    y_n = input(">Do you want to retry\n[y/n]: ")
-    if y_n == "y" | y_n == "Y":
-        loop()
-    else:
-        print("goodbye")
-"""
+loop(kans)
